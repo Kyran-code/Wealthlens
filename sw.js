9dff86eeab8a2f0f07d97943c31eaaf84da0096c
Kyran-code <kyranchong21@gmail.com>
2026-05-01 11:41:00 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wealthlens-v3';
+const CACHE = 'wealthlens-v5';
 const SHELL = ['./index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
